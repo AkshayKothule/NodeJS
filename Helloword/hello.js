364a5fd7f200591036db0218b19c7ecf2a7a9361
AkshayKothule <akshaykothule2002@gmail.com>
2025-01-20 15:57:35 +0530

    module

--- Helloword/hello.js
+++ Helloword/hello.js
@@ -1,1 +1,6 @@
-console.log("hello this is a js file");
+// console.log("hello this is a js file");
+ const {add  , sub}=require("./math")
+
+ console.log("Math value is "+add(2 , 4));
+ console.log("Math value is "+sub(2 , 4));
+ 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-test
+## NodeJS
+
+Node.js is an open-source JavaScript server environment built on Chrome’s V8 engine.
